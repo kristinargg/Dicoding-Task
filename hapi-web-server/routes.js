5edc42656a5d23434806c237e4d4ce422dc975ce
kristinargg <rggkristina26@gmail.com>
2022-11-06 17:57:22 +0700

    add

---
+++ hapi-web-server/routes.js
@@ -0,0 +1,52 @@
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
+    },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About Page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "halaman tidak dapat di akses dengan method";
+    },
+  },
+  {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+
+      if (lang === "id") {
+        return `Hai, ${name}`;
+      }
+      return `Hello, ${name}`;
+    },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "halaman tidak dapat di akses dengan method";
+    },
+  },
+];
+
+module.exports = routes;
